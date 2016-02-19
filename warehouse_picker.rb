eeb18edb2c92c97711cb925de9abe3c19acc37b6
Craig Morton <CraigMorton92@gmail.com>
2016-02-19 14:54:07 +0000

    adds function for case #2

--- warehouse_picker.rb
+++ warehouse_picker.rb
@@ -5,3 +5,8 @@ def given_bay_return_item(list_hash, bay_string)
   return item
 end
 
+def given_item_return_bay(list_hash, item_string)
+  key = list_hash.key(item_string).to_s
+
+  return key
+end
