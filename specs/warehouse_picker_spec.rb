a44cb0aaa9aa531205e4bcc8cbb5b7c940ff9c6f
Craig Morton <CraigMorton92@gmail.com>
2016-02-19 14:05:02 +0000

    Initial commit - data in hash in setup func in specs file

---
+++ specs/warehouse_picker_spec.rb
@@ -0,0 +1,49 @@
+require ("minitest/autorun")
+require_relative("../warehouse_picker.rb")
+
+class TestsWarehousePicker < MiniTest::Test
+
+  def setup
+    @product_bay_list = {
+      b7: "bath fizzers",
+      a3: "blouse",
+      a7: "bookmark",
+      c8: "candy wrapper",
+      c8: "chalk",
+      b10: "cookie jar",
+      b9: "deodorant",
+      c2: "drill press",
+      c6: "face wash",
+      a9: "glow stick",
+      a4: "hanger",
+      c10: "leg warmers",
+      a8: "model car",
+      b5: "nail filer",
+      a1: "needle",
+      c7: "paint brush",
+      b4: "photo album",
+      b3: "picture frame",
+      a10: "rubber band",
+      a5: "rubber duck",
+      c1: "rusty nail",
+      b2: "sharpie",
+      c9: "shoe lace",
+      a6: "shovel",
+      a2: "stop sign",
+      c5: "thermometer",
+      b1: "tyre swing",
+      b8: "tissue box",
+      b6: "tooth paste",
+      c4: "word search"
+    }
+  end
+
+
+
+
+    
+
+
+
+
+end  
